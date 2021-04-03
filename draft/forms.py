26888216e0364d38a590a79175e25aeb29e1c4ea
Tuan Le <sswmata123@gmail.com>
2021-04-04 02:16:00 +0700

    added date of birth in register form + fixed css

--- draft/forms.py
+++ draft/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min = 2, max = 20)])
     email = StringField('Email', validators= [DataRequired(), Email()])
+    date = DateField('Date of birth (optional) - dd/mm/YYYY', format='%d/%m/%Y', validators=(Optional(),))
     password = PasswordField('Password', validators= [DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators= [DataRequired(), EqualTo('password')])
-
+    
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
