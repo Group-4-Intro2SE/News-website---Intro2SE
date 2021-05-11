b071ad96a9bfda391ac064ca8f9ab5e0f6b42a18
Tuan Le <sswmata123@gmail.com>
2021-05-12 01:51:06 +0700

    update homepage UI

--- draft/create_new_database.py
+++ draft/create_new_database.py
@@ -3,3 +3,8 @@ from flasknews.models import User, Post
 
 
 db.create_all()
+
+# cach lay image tu local folder
+# src="{{ url_for('static', filename='profile_pics/' + post.author.image_file) }}">
+
+
